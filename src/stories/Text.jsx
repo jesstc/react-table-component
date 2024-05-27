c23ff60e829e9c797df8531aed8950fcad357966
Jing Han, Huang <7jesstc1999@gmail.com>
2024-05-27 17:42:57 +0800

    feat: add Text story in Cell Components

---
+++ src/stories/Text.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaSort, FaSortDown, FaSortUp } from "react-icons/fa";
+import { Box } from '@chakra-ui/react';
+
+export const Text = ({ content, hyperLink, longTextLines, isHeader }) => {
+  const boxStyle = {
+    padding: "2px 5px",
+    w: '100%',
+  };
+  const hyperLinkStyle = hyperLink.trim().length !== 0
+    && {
+      textDecoration: "underline",
+    };
+  const longTextStyle = longTextLines !== 0 
+    && {
+      display: '-webkit-box',
+      overflow: 'hidden',
+      wordBreak: 'break-word',
+      WebkitBoxOrient: 'vertical',
+      WebkitLineClamp: longTextLines,
+    };
+  const headerStyle = (isHeader && longTextLines === 0)
+    && {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      backgroundColor: '#F8F9FC',
+      fontWeight: "bold",
+    };
+
+  return (
+    <Box sx={{...boxStyle, ...hyperLinkStyle, ...longTextStyle, ...headerStyle}}>
+      {hyperLink.trim().length !== 0 ? 
+        (<a href={hyperLink} target="_blank" rel="noopener noreferrer">{content}</a>)
+        :
+        (content)}
+
+      {(isHeader && longTextLines === 0) && <FaSort />}
+    </Box>
+  );
+};
+
+Text.defaultProps = {
+  content: 'text',
+  hyperLink: '', 
+  longTextLines: 0,
+  isHeader: false,
+}
---
+++ src/stories/Text.stories.jsx
@@ -0,0 +1,54 @@
+// src/stories/Button.stories.tsx
+import React from 'react';
+import { Text } from './Text';
+
+export default {
+  title: 'Cell Conponents/Text',
+  component: Text,
+  argTypes: {
+    content: {
+      control: 'text',
+    },
+    hyperLink: {
+      control: 'text',
+      description: 'The url of hyper link. The text will become hyper link only if the value of `hyperLink` is not null.',
+    },
+    longTextLines: {
+      control: { type: 'number', min: 0, max: 20, step: 1 },
+      description: 'The number of ellipsis lines when the text is too long to display. The text value will be completely displayed when `longTextLines` is equal to `0`.',
+    },
+    isHeader: {
+      control: 'boolean',
+      description: 'Check if this is the header text. The header text will be `bold` and with a sorting icon on the right hand side.',
+    },
+  },
+  tags: ['autodocs'],
+};
+
+const Template = (args) => <Text {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  ...Text.defaultProps,
+};
+
+export const HyperLink = Template.bind({});
+HyperLink.args = {
+  ...Text.defaultProps,
+  content: '91APP Official Website',
+  hyperLink: 'https://www.91app.com/showcases/?campaignid=21225946208&adgroupid=161046725026&adid=697531942213&gad_source=1&gclid=Cj0KCQjw3tCyBhDBARIsAEY0XNlcfFjzxwV66SQnSRG6rkD_7JV4tYAx7JbN4MDdHZvw6Al0AIjdXZ0aAnkcEALw_wcB',
+};
+
+export const LongText = Template.bind({});
+LongText.args = {
+  ...Text.defaultProps,
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.`,
+  longTextLines: 4,
+};
+
+export const HeaderColumn = Template.bind({});
+HeaderColumn.args = {
+  ...Text.defaultProps,
+  content: '商品名稱',
+  isHeader: true,
+};

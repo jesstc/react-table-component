dc898c13a60ed620646033ff291ada58d441f8de
Jing Han, Huang <7jesstc1999@gmail.com>
2024-05-28 15:41:12 +0800

    docs: update Button description

--- src/stories/cellcomponents/Button.jsx
+++ src/stories/cellcomponents/Button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from '@chakra-ui/react'
 import * as MdIcons from 'react-icons/md';
 
@@ -19,8 +20,16 @@ export const CellButton = ({ primary, buttonColor, content, iconName, btnNum, ..
   );
 };
 
+CellButton.propTypes = {
+  content: PropTypes.string.isRequired,
+  buttonColor: PropTypes.oneOf(['gray', 'red', 'yellow', 'green', 'blue']),
+  iconName: PropTypes.string,
+  primary: PropTypes.bool,
+};
+
 CellButton.defaultProps = {
   content: '',
   buttonColor: 'blue',
   iconName: '',
+  primary: false,
 };
